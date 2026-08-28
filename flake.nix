2b77e5e4946e23b922fe48cc31a356bb3e9e7c76
Nyramu <nyramu@noreply.codeberg.org>
2026-08-28 13:19:27 +0200

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "lsfg-vk flake for the latest git master";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    import-tree.url = "github:vic/import-tree";
+  };
+
+  outputs =
+    inputs@{ flake-parts, import-tree, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (import-tree ./modules);
+}
